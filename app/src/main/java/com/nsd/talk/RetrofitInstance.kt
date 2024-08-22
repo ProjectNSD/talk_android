c2bd59997f8b70d9b40fdadb642c7fd2bee1017b
JeeSeongDeok <bh939@naver.com>
2024-08-22 09:15:57 +0900

    [feature] add retrofitInstance

---
+++ app/src/main/java/com/nsd/talk/RetrofitInstance.kt
@@ -0,0 +1,20 @@
+package com.nsd.talk
+
+import com.google.gson.Gson
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitInstance {
+    val BASE_URL ="https://faa4-211-193-229-239.ngrok-free.app/"
+
+    val client = Retrofit
+        .Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create()).build()
+
+
+    fun getInstance(): Retrofit{
+        return client
+    }
+}

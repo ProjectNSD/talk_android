05c3e53b6fa29128b9929915ffbe85653f6f7897
JeeSeongDeok <bh939@naver.com>
2024-09-03 14:03:37 +0900

    [refactoring] base url change

--- app/src/main/java/com/nsd/talk/data/RetrofitInstance.kt
+++ app/src/main/java/com/nsd/talk/data/RetrofitInstance.kt
@@ -1,15 +1,16 @@
 package com.nsd.talk.data
 
+import com.google.gson.GsonBuilder
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitInstance {
-    val BASE_URL ="https://faa4-211-193-229-239.ngrok-free.app/"
-
+    val BASE_URL ="https://ee7a-211-193-229-239.ngrok-free.app/"
+    val gson = GsonBuilder().setLenient().create()
     val client = Retrofit
         .Builder()
         .baseUrl(BASE_URL)
-        .addConverterFactory(GsonConverterFactory.create()).build()
+        .addConverterFactory(GsonConverterFactory.create(gson)).build()
 
 
     fun getInstance(): Retrofit{
